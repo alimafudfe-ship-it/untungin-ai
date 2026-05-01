08d40b25608408e3144726328f0ae229ae464dca
alimafudfe-ship-it <alimafudfe@gmail.com>
2026-05-01 14:12:44 +0700

    Commit changes
    init untungin

---
+++ supabaseClient.ts
@@ -0,0 +1,22 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+declare global {
+  var supabaseClient: ReturnType<typeof createClient> | undefined;
+}
+
+export const supabase =
+  globalThis.supabaseClient ??
+  createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+    },
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.supabaseClient = supabase;
+}
