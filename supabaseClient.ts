eb41665fe2d075c43854c54debf49b613cf05ae6
alimafudfe-ship-it <alimafudfe@gmail.com>
2026-05-01 16:46:59 +0700

    Add files via upload

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.warn("Supabase ENV belum lengkap.");
+}
 
 declare global {
   var supabaseClient: ReturnType<typeof createClient> | undefined;
